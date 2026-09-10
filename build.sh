10bd6bd896ed40c04380c74c39ecbd64f7a9b33f
Berk CAN <nrkdrk@gmail.com>
2026-09-10 13:39:18 +0300

    Bump the bundle version to 3.1

--- build.sh
+++ build.sh
@@ -55,9 +55,9 @@ cat > "$APP/Contents/Info.plist" <<PLIST
     <key>CFBundlePackageType</key>
     <string>APPL</string>
     <key>CFBundleShortVersionString</key>
-    <string>3.0.1</string>
+    <string>3.1</string>
     <key>CFBundleVersion</key>
-    <string>5</string>
+    <string>6</string>
 $ICON_KEY
     <key>LSMinimumSystemVersion</key>
     <string>13.0</string>
